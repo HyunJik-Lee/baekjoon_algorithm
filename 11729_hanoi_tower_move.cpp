e7147c606941d29a15f5ac513319f52365f87061
Hyunjik Lee <jiggy0429@gmail.com>
2021-07-22 14:38:35 +0900

    1 BFS, 2 recursion

---
+++ 11729_hanoi_tower_move.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+int N;
+
+void move(int a, int b, int n) {
+	if (n == 1) {
+		cout << a << ' ' << b << '\n';
+	}
+	else {
+		move(a, 6 - a - b, n - 1);
+		cout << a << ' ' << b << '\n';
+		move(6 - a - b, b, n - 1);
+	}
+}
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+	cin >> N;
+	cout << (1 << N) - 1 << '\n';
+	move(1, 3, N);
+}
---
+++ 13549_hideandseek3.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+int dist[100002];
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+	fill(dist, dist + 100002, -1);
+	int n, k;
+	cin >> n >> k;
+	deque<int> dq;
+	dist[n] = 0;
+	dq.push_back(n);
+	while (!dq.empty()) {
+		int cur = dq.front(); dq.pop_front();
+		if (2 * cur < 100002 && dist[2 * cur] == -1) {
+			dist[2 * cur] = dist[cur];
+			dq.push_front(2 * cur);
+		}
+		for (int nxt : {cur - 1, cur + 1}) {
+			if (nxt < 0 || nxt >= 100002 || dist[nxt] != -1) continue;
+			dist[nxt] = dist[cur] + 1;
+			dq.push_back(nxt);
+		}
+	}
+	cout << dist[k];
+
+
+}
